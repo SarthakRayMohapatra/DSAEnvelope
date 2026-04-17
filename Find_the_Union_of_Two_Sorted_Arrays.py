82f827e64016ae72c82e19c105ec22bdaa167c3e
Sarthak Ray Mohapatra <146863285+SarthakRayMohapatra@users.noreply.github.com>
2026-04-17 23:40:09 +0530

    Find The Union of Two Sorted Arrays

---
+++ Find_the_Union_of_Two_Sorted_Arrays.py
@@ -0,0 +1,35 @@
+# arr1[] = [1 1 2 3 4 5]
+# arr2[] = [2 3 4 4 5]
+
+# Union: [1 2 3 4 5]
+
+def sortedArray(a, b):
+    n1 = len(a)
+    n2 = len(b)
+
+    i = 0
+    j = 0
+
+    unionArr = []
+
+    while i < n1 and j < n2:
+        if a[i] <= b[j]:
+            if len(unionArr) == 0 or unionArr[-1] != a[i]:
+                unionArr.append(a[i])
+            i += 1
+        else:
+            if len(unionArr) == 0 or unionArr[-1] != b[j]:
+                unionArr.append(b[j])
+            j += 1
+
+    while j < n2:
+        if len(unionArr) == 0 or unionArr[-1] != b[j]:
+            unionArr.append(b[j])
+        j += 1
+
+    while i < n1:
+        if len(unionArr) == 0 or unionArr[-1] != a[i]:
+            unionArr.append(a[i])
+        i += 1
+
+    return unionArr
